f30c8e98736457dc080c7689b1ef6d16bc40a5b9
Yong Yi Wong <yongyi.fullstack@gmail.com>
2022-01-14 08:38:51 -0600

    Minimized Codes

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,6 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  HttpException,
+  HttpStatus,
+  Post,
+  Get,
+  Put,
+  Query,
+  UseGuards,
+  Request,
+  Res,
+  Header,
+} from '@nestjs/common';
 
 @Controller()
 export class AppController {
   constructor() {}
+
+  @Get('data')
+  async getData() {
+    return "hello"
+  }
 }
